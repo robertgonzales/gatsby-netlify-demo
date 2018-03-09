5300d486f4953b2ccc82277ec1ccdcca085050c0
Robert Gonzales <robertgcreative@gmail.com>
2018-03-08 19:43:00 -0500

    stuff

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,10 +1,13 @@
 import "./all.sass"
 import React from "react"
 import PropTypes from "prop-types"
+import Navbar from "../components/Navbar"
 
 const TemplateWrapper = ({ children }) => (
   <div>
+    <Navbar />
     <div>{children()}</div>
+    <footer>Hello, I'm a footer.</footer>
   </div>
 )
 
